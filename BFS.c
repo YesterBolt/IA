ad8842d8b90fd37dc5d345a4511e9e02ec43eb15
YesterBolt <32371563+YesterBolt@users.noreply.github.com>
2019-03-26 00:16:52 -0300

    Create BFS.c

---
+++ BFS.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void dfs(int grafo[20][20], int visitados[20], int vertice);
+
+int main(){
+    FILE *arquivo;
+    arquivo = fopen("matriz", "r");
+    int grafo[20][20];
+    int visitados[20];
+    int v1, v2, peso;
+
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 20; j++)
+            grafo[i][j] = -1;
+    }
+
+    while(fscanf(arquivo, "%d %d %d", &v1, &v2, &peso) != EOF){
+        grafo[v1][v2] = peso;
+        grafo[v2][v1] = peso;
+    }
+
+    for(int i = 0; i < 20; i++){
+        visitados[i] = 0;
+    }
+
+    dfs(grafo, visitados, 0);
+}
+
+void dfs(int grafo[20][20], int visitados[20], int vertice){
+    visitados[vertice] = 1;
+    if(vertice == 11) return;
+
+    for(int i = 0; i < 20; i++){
+        if(visitados[grafo[vertice][i]] == -1){
+            dfs(grafo, visitados, i);
+        }
+    }
+}
