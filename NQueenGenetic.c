465f6537cfa8256a0cbec2f1cb4d4778ed97093d
YesterBolt <32371563+YesterBolt@users.noreply.github.com>
2019-04-10 14:41:09 -0300

    Update NQueenGenetic.c

--- NQueenGenetic.c
+++ NQueenGenetic.c
@@ -3,9 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-#define MAXINT 1000000
 #define NUMQUEEN 4
-#define TABU NUMQUEEN/2
+#define NUMPOP 5
 
 typedef struct populacao{
     int QueenList[NUMQUEEN];
@@ -16,22 +15,23 @@ typedef struct populacao{
 //HEURISTICA E A QUANTIDADE DE RAINHAS EM CONFLITOS - 1
 
 int verificQueen(int QueenList[NUMQUEEN]); //FUNCAO RESPONSAVEL POR VERIFICAR SE AS RAINHAS ESTAO EM CONFLITOS
-int solution(int QueenList[NUMQUEEN], Populacao populacao[5]);
-int shattering(Populacao populacao[5]);
+int solution(int QueenList[NUMQUEEN], Populacao populacao[NUMPOP]); //FUNCAO QUE RESOLVE O PROBLEMA
+int shattering(Populacao populacao[NUMPOP]); //FUNCAO RESPONSAVEL POR SELECIONAR UMA DAS POPULACOES
 
 int main(){
     int QueenList[NUMQUEEN];    //POSICAO DAS RAINHAS, CADA RAINHA PERTENCE A UMA COLUNA NO QUAL O VALOR PRESENTE REPRESENTA A LINHA EM QUE ELA ESTA POSICIONADA
     int ConflictQueen, aux;          //VARIAVEL PARA CONTROLE DE QUANTIDADES DE RAINHAS EM CONFLITO
-    Populacao populacao[5];
+    Populacao populacao[NUMPOP];
     srand(time(NULL));
 
-    //FOR USADO PARA INICIAR AS RAINHAS COLOCANDO TODAS NA MESMA LINHA, NO CASO LINHA 0
-    for(int i = 0; i < 5; i++){
+    //FOR USADO PARA INICIAR AS POPULACOES, COLOCANDO AS RAINHAS DE FORMA ALEATORIA
+    for(int i = 0; i < NUMPOP; i++){
         for(int j = 0; j < NUMQUEEN; j++)
             populacao[i].QueenList[j] = rand()%NUMQUEEN;
         populacao[i].ConflictQueen = verificQueen(populacao[i].QueenList);
     }
 
+    //CHAMADA DA FUNCAO
     int interacoes = solution(QueenList, populacao);
     for(int i = 0; i < NUMQUEEN; i++)
         printf("%d ", QueenList[i]);
@@ -40,50 +40,47 @@ int main(){
     return 0;
 }
 
-int shattering(Populacao populacao[5]){
+int shattering(Populacao populacao[NUMPOP]){
+    //PRE: CONJUNTO DE POPULACAO
+    //POS: POSICAO SELECIONADA
     int i, aux = 0, selection;
-    int auxVet[5];
+    int auxVet[NUMPOP];
 
-    for(i = 0; i < 5; i++){
+    //AUX E USADO PARA AMAZENAR O TOTAL DE RAINHAS EM CONFLITO
+    for(i = 0; i < NUMPOP; i++){
         aux += populacao[i].ConflictQueen;
     }
-    //printf("aux1 = %d\n", aux);
+    //OPERACAO UTILIZADA PARA ESCALAR O PESO DAS RAINHAS EM CONFLITO
     auxVet[0] = (aux - populacao[0].ConflictQueen) ;
-    for(i = 1; i < 5; i++){
+    for(i = 1; i < NUMPOP; i++){
         auxVet[i] = auxVet[i-1] + (aux - populacao[i].ConflictQueen);
     }
-    aux = auxVet[4];
-    //printf("aux = %d\n", aux);
+    aux = auxVet[NUMPOP - 1];
     selection = rand()%aux;
 
-    //printf("%d Selection\n", selection);
-    if(selection < auxVet[0])
-        return 0;
-    else if(selection >= auxVet[0] && selection < auxVet[1])
-        return 1;
-    else if(selection >= auxVet[1] && selection < auxVet[2])
-        return 2;
-    else if(selection >= auxVet[2] && selection < auxVet[3])
-        return 3;
-    else if(selection >= auxVet[3])
-        return 4;
+    for(int i = 0; i < NUMPOP; i++){
+        if(selection < auxVet[i])
+            return i;
+    }
 
 }
 
-int solution(int QueenList[NUMQUEEN], Populacao populacao[5]){
-    Populacao populacaoAux[5], newPopulacao[5];
+int solution(int QueenList[NUMQUEEN], Populacao populacao[NUMPOP]){
+    //PRE: ARRAY AONDE SERA ARMAZENADO A SOLUCAO E UM CONJUNTO DE POPULACOES
+    //POS: SOLUCAO E ARMAZENADA EM QUEENLIST E RETORNA O NUMERO DE INTERACOES
+    Populacao populacaoAux[NUMPOP], newPopulacao[NUMPOP];
     int aux, random, mutation = 10, count = 0;
 
     while(1){
         count++;
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < NUMPOP; i++){
             aux = shattering(populacao);
             memcpy(populacaoAux[i].QueenList, populacao[i].QueenList, sizeof(int)*NUMQUEEN);
             populacaoAux[i].ConflictQueen = populacao[aux].ConflictQueen;
         }
 
         //CROSSOVER
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < NUMPOP; i++){
             aux = shattering(populacaoAux);
             for(int j = 0; j < NUMQUEEN/2; j++){
                 newPopulacao[i].QueenList[j] = populacaoAux[aux].QueenList[j];
@@ -95,7 +92,7 @@ int solution(int QueenList[NUMQUEEN], Populacao populacao[5]){
         }
 
         //MUTACAO
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < NUMPOP; i++){
             aux = rand()%mutation;
             if(aux == 0){
                 mutation = 10;
@@ -106,13 +103,14 @@ int solution(int QueenList[NUMQUEEN], Populacao populacao[5]){
         }
 
         //ATUALIZA STATUS DA POPULACAO
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < NUMPOP; i++){
             aux = shattering(populacao);
             memcpy(populacao[i].QueenList, newPopulacao[i].QueenList, sizeof(int)*NUMQUEEN);
             populacao[i].ConflictQueen = verificQueen(populacao[i].QueenList);
         }
 
-        for(int i = 0; i < 5; i++)
+        //CASO ENCONTRE UMA SOLUCAO RETORNE
+        for(int i = 0; i < NUMPOP; i++)
             if(populacao[i].ConflictQueen == 0){
                 memcpy(QueenList, populacao[i].QueenList, sizeof(int)*NUMQUEEN);
                 return count;
